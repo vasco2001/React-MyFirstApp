c9b4cb50a6e822edab20093bc26e0792e5f9f03f
vasco2001 <aluno23055@ipt.pt>
2022-05-30 14:39:54 +0100

    Algumas alterações irelevantes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+/**
+ * App.js 
+ */
+
 import logo from './logo.svg';
 import './App.css';
 
+// there are two types of components:
+//  -class
+//  -function
 function App() {
+  // return is mandatory
   return (
+    // return have only ONE object
     <div className="App">
       <header className="App-header">
+        {/* todas as variaveis são envolvidas por chavetas */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +28,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <p>
+          Here,  we are going to do something useful...
+        </p>
+      </div>
+      <footer>
+        &copy; Vasco
+      </footer>
     </div>
   );
 }
